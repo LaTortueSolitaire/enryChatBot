f3983778a2e77a12d78bce4e49616e8481d04c63
Lucas Grellier <Lucas.Grellier@grenoble-inp.org>
2019-03-21 18:59:26 +0100

    add config

---
+++ config/settings.js
@@ -0,0 +1,17 @@
+var settings = {
+    www: {
+        port: 3001
+    },
+    db: {
+        host: 'localhost',
+        port: 27017,
+        name: 'enryChat'
+    },
+    facebook: {
+        fb_page_token: "",
+        fb_verify_token: "",
+        fb_app_secret: ""
+    }
+};
+
+module.exports = settings;
