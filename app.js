35642cc714d498b9f21d2e0c519521c6b5480f35
LaTortueSolitaire <perfor971@gmail.com>
2019-03-28 14:42:13 +0100

    same

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var db = require('db/db.js');
+var db = require('./db/db.js');
 
 var axios = require('axios');
 
